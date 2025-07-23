0c834f07a5f3a60ee73c1ac1517d04ad63f82690
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-23 14:17:45 +0000

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from '@/components/layout/Header';
 import PostCard from '../components/common/PostCard';
 import { PostProps } from '../interfaces';
-import { GetStaticProps } from 'next';
+import { getStaticProps } from 'next';
 
 const Posts = () => {
   const [posts, setPosts] = useState<PostProps[]>([]);

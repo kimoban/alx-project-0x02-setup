446fc707261756b0ca637fd6301a62268a11da73
Isaiah Kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-23 14:12:27 +0000

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/layout/Header';
+import Header from '@/components/layout/Header';
 import PostCard from '../components/common/PostCard';
 import { PostProps } from '../interfaces';
 
